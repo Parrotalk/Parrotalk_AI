72c91997cafb8811453dafa469cacee724691fcb
JiwonHwang84 <ghkdwldnjs112@gmail.com>
2024-12-16 17:41:17 +0900

    fix: 400에러 및 잘못된 입력시 요약기능 중 빈배열 반환

--- model/summary_dialogue_prompt.py
+++ model/summary_dialogue_prompt.py
@@ -12,6 +12,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 # 템플릿 문자열 정의
 template_string = """
 작업: 두명이 대화한 다음 대화록을 요약하고 대화록에 나온 '나'가 해야할 해야할일을 리스트화 해라
+대화내용이 아무것도 없거나 요약이 불필요할 경우 summary는 빈배열을 반환
 
 summary: 요약문
 todo: 해야할일 목록
